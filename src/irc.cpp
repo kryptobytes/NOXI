94f1c1a90c1b307db857c2a620c5e2f823ff1c73
root <root@normansland1.iglobe.be>
2014-07-10 15:41:44 +0200

    All names changed to NOXI

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #xcoin2TEST3\r");
-            Send(hSocket, "WHO #xcoin2TEST3\r");
+            Send(hSocket, "JOIN #NOXI2TEST3\r");
+            Send(hSocket, "WHO #NOXI2TEST3\r");
         } else {
-            // randomly join #xcoin00-#xcoin99
+            // randomly join #NOXI00-#NOXI99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #xcoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #xcoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #NOXI2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #NOXI2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -37,7 +37,7 @@ Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
         throw runtime_error(
-            "importprivkey <xcoin private key> [label]\n"
+            "importprivkey <NOXI private key> [label]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -74,13 +74,13 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-	    "dumpprivkey <xcoin address>\n"
-	    "Reveals the private key corresponding to <xcoin address>.");
+	    "dumpprivkey <NOXI address>\n"
+	    "Reveals the private key corresponding to <NOXI address>.");
 
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-	throw JSONRPCError(-5, "Invalid XCoin address");
+	throw JSONRPCError(-5, "Invalid NOXI address");
     CKeyID keyID;
     if (!address.GetKeyID(keyID))
         throw JSONRPCError(-3, "Address does not refer to a key");
